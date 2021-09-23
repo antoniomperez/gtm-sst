609b6c1357dcbae8359ae774ceec12e82d9812c4
Antonio Márquez Pérez <amarquez1980@gmail.com>
2021-09-23 23:24:23 +0200

    WIP: woocommerce installed

--- apps/wordpress/html/wp-content/themes/twentytwenty/header.php
+++ apps/wordpress/html/wp-content/themes/twentytwenty/header.php
@@ -20,11 +20,28 @@
 
 		<link rel="profile" href="https://gmpg.org/xfn/11">
 
+		<!-- Google Tag Manager -->
+		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+			new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+			j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+			'https://www.googletagmanager.com/gtm.js?id='+i+dl+   '&gtm_auth=R2hZsn8VECXhbNL609uSdg&gtm_preview=env-3&gtm_cookies_win=x';f.parentNode.insertBefore(j,f);
+			})(window,document,'script','dataLayer','GTM-TVNG8V7');
+		</script>
+		<!-- End Google Tag Manager -->
+
 		<?php wp_head(); ?>
 
 	</head>
 
 	<body <?php body_class(); ?>>
+
+		<!-- Google Tag Manager (noscript) -->
+		<noscript>
+			<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TVNG8V7&gtm_auth=R2hZsn8VECXhbNL609uSdg&gtm_preview=env-3&gtm_cookies_win=x"
+			height="0" width="0" style="display:none;visibility:hidden">
+			</iframe>
+		</noscript>
+		<!-- End Google Tag Manager (noscript) -->	
 
 		<?php
 		wp_body_open();
--- apps/wordpress/html/wp-content/themes/twentytwentyone/header.php
+++ apps/wordpress/html/wp-content/themes/twentytwentyone/header.php
@@ -17,10 +17,27 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+			new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+			j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+			'https://www.googletagmanager.com/gtm.js?id='+i+dl+   '&gtm_auth=R2hZsn8VECXhbNL609uSdg&gtm_preview=env-3&gtm_cookies_win=x';f.parentNode.insertBefore(j,f);
+			})(window,document,'script','dataLayer','GTM-TVNG8V7');
+		</script>
+		<!-- End Google Tag Manager -->
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
+	  <!-- Google Tag Manager (noscript) -->
+		<noscript>
+			<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TVNG8V7&gtm_auth=R2hZsn8VECXhbNL609uSdg&gtm_preview=env-3&gtm_cookies_win=x"
+			height="0" width="0" style="display:none;visibility:hidden">
+			</iframe>
+		</noscript>
+		<!-- End Google Tag Manager (noscript) -->	
+
 <?php wp_body_open(); ?>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'twentytwentyone' ); ?></a>
